026935291f0897db9ae7bd38ffe6e4491d6225f0
KJ <tongkj2000@gmail.com>
2022-09-17 00:08:05 +0800

    Added utility.py which handles encoding of the bits.
    Updated mp3_steg to use different number of bits. Requires fixing to handle remainder bits & message sometimes comes back garbled.

--- mp3_steg.py
+++ mp3_steg.py
@@ -1,4 +1,5 @@
 import binascii
+import utility
 
 
 def encode_message(list_of_hex: list, message: str, num_bits: int = 1):
@@ -8,15 +9,109 @@ def encode_message(list_of_hex: list, message: str, num_bits: int = 1):
     message = ''.join(f"{ord(i):08b}" for i in message)
     length_of_message = len(message)
     for i in range(len(list_of_hex)):
-        if list_of_hex[i] == b'ff':
-            if num_bits == 1:
-                if message_counter < length_of_message:
+        if message_counter < length_of_message:
+            if list_of_hex[i] == b'ff':
+                if num_bits == 1:
                     current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
-                    replaced_byte = bytes(current_byte[:-1] + message[message_counter], 'utf8')  # Replaces bit 24 (
-                    # private bit) with message bit
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 24
                     message_counter += 1
-                    list_of_hex[i + 2] = bytes(f'{int(replaced_byte, 2):x}', 'utf8').zfill(2)  # padding to ensure 2
-                    # digits
+                    list_of_hex[i + 2] = utility.convert_bin_to_hex(replaced_byte)
+                elif num_bits == 2:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 24
+                    message_counter += 1
+                    list_of_hex[i + 2] = utility.convert_bin_to_hex(replaced_byte)
+                    # Encodes the next part of the header (Bits 25-32). As bit 27 and 28 affects the stereo, it will
+                    # be encoded last
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 32
+                    message_counter += 1
+                    list_of_hex[i + 3] = utility.convert_bin_to_hex(replaced_byte)
+                elif num_bits == 3:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 24
+                    message_counter += 1
+                    list_of_hex[i + 2] = utility.convert_bin_to_hex(replaced_byte)
+                    # Encodes the next part of the header (Bits 25-32). As bit 27 and 28 affects the stereo, it will
+                    # be encoded last
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 32
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_1(replaced_byte, message[message_counter])  # Replace bit 31
+                    message_counter += 1
+                    list_of_hex[i + 3] = utility.convert_bin_to_hex(replaced_byte)
+                elif num_bits == 4:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 24
+                    message_counter += 1
+                    list_of_hex[i + 2] = utility.convert_bin_to_hex(replaced_byte)
+                    # Encodes the next part of the header (Bits 25-32). As bit 27 and 28 affects the stereo, it will
+                    # be encoded last
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 32
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_1(replaced_byte, message[message_counter])  # Replace bit 31
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_2(replaced_byte, message[message_counter])  # Replace bit 30
+                    message_counter += 1
+                    list_of_hex[i + 3] = utility.convert_bin_to_hex(replaced_byte)
+                elif num_bits == 5:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 24
+                    message_counter += 1
+                    list_of_hex[i + 2] = utility.convert_bin_to_hex(replaced_byte)
+                    # Encodes the next part of the header (Bits 25-32). As bit 27 and 28 affects the stereo, it will
+                    # be encoded last
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 32
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_1(replaced_byte, message[message_counter])  # Replace bit 31
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_2(replaced_byte, message[message_counter])  # Replace bit 30
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_3(replaced_byte, message[message_counter])  # Replace bit 29
+                    message_counter += 1
+                    list_of_hex[i + 3] = utility.convert_bin_to_hex(replaced_byte)
+                elif num_bits == 6:  # Expect audio difference from here on out
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 24
+                    message_counter += 1
+                    list_of_hex[i + 2] = utility.convert_bin_to_hex(replaced_byte)
+                    # Encodes the next part of the header (Bits 25-32). As bit 27 and 28 affects the stereo, it will
+                    # be encoded last
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 32
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_1(replaced_byte, message[message_counter])  # Replace bit 31
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_2(replaced_byte, message[message_counter])  # Replace bit 30
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_3(replaced_byte, message[message_counter])  # Replace bit 29
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_4(replaced_byte, message[message_counter])  # Replace bit 28
+                    message_counter += 1
+                    list_of_hex[i + 3] = utility.convert_bin_to_hex(replaced_byte)
+                elif num_bits == 7:  # Expect audio difference from here on out
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 24
+                    message_counter += 1
+                    list_of_hex[i + 2] = utility.convert_bin_to_hex(replaced_byte)
+                    # Encodes the next part of the header (Bits 25-32). As bit 27 and 28 affects the stereo, it will
+                    # be encoded last
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    replaced_byte = utility.encode_bit_0(current_byte, message[message_counter])  # Replace bit 32
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_1(replaced_byte, message[message_counter])  # Replace bit 31
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_2(replaced_byte, message[message_counter])  # Replace bit 30
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_3(replaced_byte, message[message_counter])  # Replace bit 29
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_4(replaced_byte, message[message_counter])  # Replace bit 28
+                    message_counter += 1
+                    replaced_byte = utility.encode_bit_5(replaced_byte, message[message_counter])  # Replace bit 27
+                    message_counter += 1
+                    list_of_hex[i + 3] = utility.convert_bin_to_hex(replaced_byte)
 
 
 def decode_message(list_of_hex: list, len_message: int, num_bits: int = 1):
@@ -32,6 +127,78 @@ def decode_message(list_of_hex: list, len_message: int, num_bits: int = 1):
                     current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
                     binary_list.append(current_byte[-1])  # inserts bit 24 into list
                     current_len += 1
+                elif num_bits == 2:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    binary_list.append(current_byte[-1])  # inserts bit 24 into list
+                    current_len += 1
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    binary_list.append(current_byte[-1])  # inserts bit 32 into list
+                    current_len += 1
+                elif num_bits == 3:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    binary_list.append(current_byte[-1])  # inserts bit 24 into list
+                    current_len += 1
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    binary_list.append(current_byte[-1])  # inserts bit 32 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-2])  # inserts bit 31 into list
+                    current_len += 1
+                elif num_bits == 4:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    binary_list.append(current_byte[-1])  # inserts bit 24 into list
+                    current_len += 1
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    binary_list.append(current_byte[-1])  # inserts bit 32 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-2])  # inserts bit 31 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-3])  # inserts bit 30 into list
+                    current_len += 1
+                elif num_bits == 5:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    binary_list.append(current_byte[-1])  # inserts bit 24 into list
+                    current_len += 1
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    binary_list.append(current_byte[-1])  # inserts bit 32 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-2])  # inserts bit 31 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-3])  # inserts bit 30 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-4])  # inserts bit 29 into list
+                    current_len += 1
+                elif num_bits == 6:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    binary_list.append(current_byte[-1])  # inserts bit 24 into list
+                    current_len += 1
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    binary_list.append(current_byte[-1])  # inserts bit 32 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-2])  # inserts bit 31 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-3])  # inserts bit 30 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-4])  # inserts bit 29 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-5])  # inserts bit 28 into list
+                    current_len += 1
+                elif num_bits == 7:
+                    current_byte = bin(int(list_of_hex[i + 2], 16))[2:].zfill(8)  # Gets the next 8 bits (17-24)
+                    binary_list.append(current_byte[-1])  # inserts bit 24 into list
+                    current_len += 1
+                    current_byte = bin(int(list_of_hex[i + 3], 16))[2:].zfill(8)  # Gets the next 8 bits (25-32)
+                    binary_list.append(current_byte[-1])  # inserts bit 32 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-2])  # inserts bit 31 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-3])  # inserts bit 30 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-4])  # inserts bit 29 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-5])  # inserts bit 28 into list
+                    current_len += 1
+                    binary_list.append(current_byte[-6])  # inserts bit 27 into list
+                    current_len += 1
             else:
                 break
     return binary_list
@@ -45,26 +212,26 @@ def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
 
 
-def write_secret_to_file(filename: str, secret: str):
-    """Function takes in the filename of mp3 file and the secret to be encoded inside. Writes the secret message
-    into a new file called secret.mp3 which is created in the same directory"""
+def write_secret_to_file(filename: str, secret: str, num_bits: int = 1):
+    """Function takes in the filename of mp3 file, secret to be encoded inside and number of bits to use.
+    Writes the secret message into a new file called secret.mp3 which is created in the same directory"""
     with open(filename, 'rb') as fileread:
         encodehexstring = binascii.hexlify(fileread.read())
 
     encodelist = [encodehexstring[i:i + 2] for i in range(0, len(encodehexstring), 2)]
-    encode_message(encodelist, secret)
+    encode_message(encodelist, secret, num_bits)
 
     with open('secret.mp3', 'wb') as filewrite:
         filewrite.write(binascii.unhexlify(b''.join(encodelist)))
 
 
-def get_secret_from_file(filename: str, msg_len: int):
-    """Function takes in the filename of the mp3 file which has a secret inside and the length of the secret message
-    (in number of characters). Returns the decoded secret"""
+def get_secret_from_file(filename: str, msg_len: int, num_bits: int = 1):
+    """Function takes in the filename of the mp3 file which has a secret inside, the length of the secret message
+    (in number of characters) and number of bits to use. Returns the decoded secret"""
     with open(filename, 'rb') as f:
         decodehexstring = binascii.hexlify(f.read())
 
     decodelist = [decodehexstring[i:i + 2] for i in range(0, len(decodehexstring), 2)]
-    decoded_byte = decode_message(decodelist, msg_len)
+    decoded_byte = decode_message(decodelist, msg_len, num_bits)
     decoded_string = ''.join(decoded_byte)
     return text_from_bits(decoded_string)
---
+++ utility.py
@@ -0,0 +1,59 @@
+def encode_bit_0(bin_str: str, bit: str):
+    """Function takes in the byte (in terms of binary) to manipulate and a bit to encode (1 or 0). Changes bit 0.
+     Returns the encoded value as a string """
+    replaced_byte = bin_str[:-1] + bit  # Replaces bit 0 aka LSB
+    return replaced_byte  # padding to ensure 2 digits
+
+
+def encode_bit_1(bin_str: str, bit: str):
+    """Function takes in the byte (in terms of binary) to manipulate and a bit to encode (1 or 0). Changes bit 1.
+     Returns the encoded value as a string """
+    replaced_byte = bin_str[:-2] + bit + bin_str[-1]  # Encodes bit 1
+    return replaced_byte
+
+
+def encode_bit_2(bin_str: str, bit: str):
+    """Function takes in the byte (in terms of binary) to manipulate and a bit to encode (1 or 0). Changes bit 2.
+     Returns the encoded value as a string """
+    replaced_byte = bin_str[:-3] + bit + bin_str[-2:]  # Encodes bit 2
+    return replaced_byte  # padding to ensure 2 digits
+
+
+def encode_bit_3(bin_str: str, bit: str):
+    """Function takes in the byte (in terms of binary) to manipulate and a bit to encode (1 or 0). Changes bit 3.
+     Returns the encoded value as a string """
+    replaced_byte = bin_str[:-4] + bit + bin_str[-3:]  # Encodes bit 3
+    return replaced_byte  # padding to ensure 2 digits
+
+
+def encode_bit_4(bin_str: str, bit: str):
+    """Function takes in the byte (in terms of binary) to manipulate and a bit to encode (1 or 0). Changes bit 4.
+     Returns the encoded value as a string """
+    replaced_byte = bin_str[:-5] + bit + bin_str[-4:]  # Encodes bit 4
+    return replaced_byte  # padding to ensure 2 digits
+
+
+def encode_bit_5(bin_str: str, bit: str):
+    """Function takes in the byte (in terms of binary) to manipulate and a bit to encode (1 or 0). Changes bit 5.
+     Returns the encoded value as a string """
+    replaced_byte = bin_str[:-6] + bit + bin_str[-5:]  # Encodes bit 5
+    return replaced_byte  # padding to ensure 2 digits
+
+
+def encode_bit_6(bin_str: str, bit: str):
+    """Function takes in the byte (in terms of binary) to manipulate and a bit to encode (1 or 0). Changes bit 6.
+     Returns the encoded value as a string """
+    replaced_byte = bin_str[:-7] + bit + bin_str[-6:]  # Encodes bit 6
+    return replaced_byte  # padding to ensure 2 digits
+
+
+def encode_bit_7(bin_str: str, bit: str):
+    """Function takes in the byte (in terms of binary) to manipulate and a bit to encode (1 or 0). Changes bit 7.
+     Returns the encoded value as a string """
+    replaced_byte = bin_str[:-8] + bit + bin_str[-7:]  # Encodes bit 7
+    return replaced_byte  # padding to ensure 2 digits
+
+
+def convert_bin_to_hex(bin_str: str):
+    """Takes in a binary string (string of 1's and 0's) and formats it into the hexadecimal value"""
+    return bytes(f'{int(bin_str, 2):x}', 'utf8').zfill(2)  # padding to ensure 2 digits
