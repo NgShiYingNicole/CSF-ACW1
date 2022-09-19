708e92c96251922f1bc3173e10a0253673d019b9
KJ <tongkj2000@gmail.com>
2022-09-19 12:58:27 +0800

    Added basic file and function selection. Todo: Integrate logic

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk, filedialog, messagebox
 
 
-def select_file():
+def select_file(sv: StringVar):
     filetypes = (
         ('Image files', ('*.png', '*.bmp')),
         ('All files', '*.*')
@@ -13,21 +13,49 @@ def select_file():
         initialdir='/',
         filetypes=filetypes)
 
-    messagebox.showinfo(
-        title='Selected File',
-        message=filename
-    )
+    try:
+        sv.set(filename)
+    except:
+        messagebox.showinfo('Error', message='Invalid file selection')
 
 
 root = Tk()
 root.title('CSF-AWC1')
-root.geometry('640x400')
-menubar = Menu(root)  # creates the menubar
-root.config(menu=menubar)
-file_menu = Menu(menubar)  # creates a menu
-file_menu.add_command(label='Open', command=select_file)  # add open file command to menubar
-file_menu.add_command(label='Exit', command=root.destroy)  # add exit command to menubar
-menubar.add_cascade(label='File', menu=file_menu)  # add menubar to menu
+root.geometry('800x600')
+# Variable declarations
+selected_cover_file_stringvar = StringVar(root)
+selected_payload_file_stringvar = StringVar(root)
+function_selected = StringVar(root)
+
+# Frame creation
 frame = ttk.Frame(root, padding=10)
 frame.grid()
+
+# Labels
+cover_file_selection_label = Label(root, text='Cover file selected: ')
+cover_selected_file_label = Label(root, textvariable=selected_cover_file_stringvar)
+payload_file_selection_label = Label(root, text='Payload file selected: ')
+payload_selected_file_label = Label(root, textvariable=selected_payload_file_stringvar)
+function_selection = Label(root, text='Select function: ')
+function_selection_dropdown = OptionMenu(root, function_selected, 'Encrypt', 'Decrypt')
+
+# Buttons
+select_cover_file_button = Button(root, text='Choose cover file:',
+                                  command=lambda: select_file(selected_cover_file_stringvar))
+select_payload_file_button = Button(root, text='Choose cover file:',
+                                    command=lambda: select_file(selected_payload_file_stringvar))
+try_start_function_button = Button(root, text='Begin!',
+                                   command='')
+
+# Grid alignment
+cover_file_selection_label.grid(row=0, column=0, sticky=W, pady=2, padx=2)
+cover_selected_file_label.grid(row=0, column=1, sticky=W, pady=2, padx=2)
+select_cover_file_button.grid(row=0, column=2, sticky=E, pady=2, padx=2)
+payload_file_selection_label.grid(row=1, column=0, sticky=W, pady=2, padx=2)
+payload_selected_file_label.grid(row=1, column=1, sticky=W, pady=2, padx=2)
+select_payload_file_button.grid(row=1, column=2, sticky=E, pady=2, padx=2)
+function_selection.grid(row=2, column=0, sticky=W, pady=2, padx=2)
+function_selection_dropdown.grid(row=2, column=2, sticky=E, pady=2, padx=2)
+try_start_function_button.grid(row=3, column=2, sticky=E, pady=2, padx=2)
+# Main loop
 root.mainloop()
